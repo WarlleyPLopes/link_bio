2b6d68bcd336255a6b2d60445e885cef007a6b12
Warlley Lopes <warlleylopes123@gmail.com>
2024-04-19 00:40:12 +0100

    media query correction

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,25 +1,26 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
- * {
-         margin: 0;
-         padding: 0;
-         box-sizing: border-box;
-     } 
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    } 
 
-body {
+    body {
         background: url(${({ theme }) => theme.COLORS.Background_mobile}) no-repeat top center/cover;
         color: ${({ theme }) => theme.COLORS.Text};
-
         -webkit-font-smoothing: antialiased;
     } 
 
     body * {
-         font-family: "Inter", sans-serif;
-         color: ${({ theme }) => theme.COLORS.Text};
-         }
+        font-family: "Inter", sans-serif;
+        color: ${({ theme }) => theme.COLORS.Text};
+    }
 
-         @media (min_width:700px) {
+    @media (min-width: 700px) {
+        body {
             background: url(${({ theme }) => theme.COLORS.Background}) no-repeat top center/cover;
-         }
+        }
+    }
 `;
